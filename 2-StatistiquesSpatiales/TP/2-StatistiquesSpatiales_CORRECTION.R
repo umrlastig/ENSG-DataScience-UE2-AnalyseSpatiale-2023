e75a419954247eaeb5b106cde94c739cbc4185b2
Juste Raimbault <juste.raimbault@ign.fr>
2023-11-20 14:32:10 +0100

    tp_2

--- 2-StatistiquesSpatiales/TP/2-StatistiquesSpatiales_CORRECTION.R
+++ 2-StatistiquesSpatiales/TP/2-StatistiquesSpatiales_CORRECTION.R
@@ -13,9 +13,23 @@ library(mapsf)
 
 rawdvf2021 <- read_csv(file = 'https://files.data.gouv.fr/geo-dvf/latest/csv/2021/full.csv.gz')
 
+toremove=unique(rawdvf2021[!is.na(rawdvf2021$valeur_fonciere)&
+                    rawdvf2021$valeur_fonciere>200000000,c("id_mutation")])
 
 # agrégation au niveau départemental (via code_departement)
+depdvf = rawdvf2021 %>%
+  filter(type_local%in%c('Appartement', 'Maison')) %>%
+  filter(!id_mutation%in%unlist(toremove)) %>%
+  group_by(code_departement) %>% 
+  summarise(
+    prix = median(valeur_fonciere, na.rm=T),
+    surface_bati = median(surface_reelle_bati, na.rm=T),
+    surface_terrain = median(surface_terrain, na.rm=T)
+  )
 
+summary(depdvf$prix)
+
+  
 
 # autres données
 
@@ -27,7 +41,12 @@ popdeps = read_delim('../../1-AnalyseSpatiale/TP/data/insee/Departements.csv', d
 insee_filosofi <- read_delim(file = 'data/filosofi/cc_filosofi_2020_DEP.csv', delim = ";")
 
 # jointure
-
+deps = left_join(deps,popdeps[,c("CODDEP","PTOT")],
+                 by=c("CODE_DEPT"="CODDEP"))
+deps = left_join(deps,insee_filosofi[,c("CODGEO","MED20","PPEN20","PPAT20")],
+                 by=c("CODE_DEPT"="CODGEO"))
+deps = left_join(deps,depdvf,by=c("CODE_DEPT"="code_departement"))
+deps = na.exclude(deps)
 
 # exploration des données: cartes
 
@@ -37,7 +56,6 @@ insee_filosofi <- read_delim(file = 'data/filosofi/cc_filosofi_2020_DEP.csv', de
 
 
 # modele lineaire non spatialise
-
 
 
 # cartographier résidus du modèle linéaire
